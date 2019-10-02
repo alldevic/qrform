c2c22acc478ec2abae05000af2817d7e7c912e6f
DOXOPOKC <=>
2019-10-02 13:07:24 +0700

    Change vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Qr</span>
-        <span class="font-weight-light">Form</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        v-for="item in menu"
-        text
-        :to="item.link"
-      >
-        <span class="mr-2">{{item.title}}</span>
-      </v-btn>
-    </v-app-bar>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>Qr</span>
+                <span class="font-weight-light">Form</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn
+                    v-for="item in menu"
+                    text
+                    :to="item.link"
+            >
+                <span class="mr-2">{{ item.title }}</span>
+            </v-btn>
+        </v-app-bar>
 
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data: () => ({
-    menu: [
-      {
-        title: "Home",
-        link: "/"
-      },
-      {
-        title: "About",
-        link: "/about"
-      },
-    ]
-  }),
+    name: "App",
+    data: () => ({
+        menu: [
+            {
+                title: "Home",
+                link: "/"
+            },
+            {
+                title: "About",
+                link: "/about"
+            },
+        ]
+    }),
 };
 </script>
